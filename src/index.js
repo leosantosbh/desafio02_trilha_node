29dc96e38b6a4f57aa9fe7e6fd56dcb08c8bc458
leosantosbh <soundsystem07@gmail.com>
2021-03-11 23:13:47 -0300

    validação testes nodejs

--- src/index.js
+++ src/index.js
@@ -10,19 +10,78 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'Username already exists' });
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next()
+  }
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'sem permissão' });
+  }
+
+  return next()
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'id informado inválido' });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'usuário não encontrado' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo não encontrado' });
+  }
+
+  request.user = user;
+
+  request.todo = todo;
+  
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.id === id);
+
+  if(!userExists) {
+    return response.status(404).json({ error: 'usuario não encontrado' });
+  }
+
+  request.user = userExists
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
